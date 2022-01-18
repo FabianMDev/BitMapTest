1cd2bf01932ddd17bd4a8bcb2e969b5613ba5ad8
Fabian Menghin <Fabian.Menghin@itplus.at>
2022-01-18 17:34:06 +0100

    work work work...

--- BitMapTest/Form1.Designer.cs
+++ BitMapTest/Form1.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.panelSideMenu = new System.Windows.Forms.Panel();
+            this.buttonColorPoints = new System.Windows.Forms.Button();
+            this.buttonColorDots = new System.Windows.Forms.Button();
+            this.buttonColorBackground = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxAnimation = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.buttonResetBM = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.PointsCount = new System.Windows.Forms.Label();
@@ -44,6 +52,14 @@
             // 
             // panelSideMenu
             // 
+            this.panelSideMenu.Controls.Add(this.buttonColorPoints);
+            this.panelSideMenu.Controls.Add(this.buttonColorDots);
+            this.panelSideMenu.Controls.Add(this.buttonColorBackground);
+            this.panelSideMenu.Controls.Add(this.button1);
+            this.panelSideMenu.Controls.Add(this.checkBoxAnimation);
+            this.panelSideMenu.Controls.Add(this.checkBox1);
+            this.panelSideMenu.Controls.Add(this.buttonResetBM);
+            this.panelSideMenu.Controls.Add(this.buttonReset);
             this.panelSideMenu.Controls.Add(this.numericUpDown1);
             this.panelSideMenu.Controls.Add(this.checkedListBox1);
             this.panelSideMenu.Controls.Add(this.PointsCount);
@@ -54,6 +70,100 @@
             this.panelSideMenu.Name = "panelSideMenu";
             this.panelSideMenu.Size = new System.Drawing.Size(185, 674);
             this.panelSideMenu.TabIndex = 0;
+            // 
+            // buttonColorPoints
+            // 
+            this.buttonColorPoints.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonColorPoints.Location = new System.Drawing.Point(8, 269);
+            this.buttonColorPoints.Name = "buttonColorPoints";
+            this.buttonColorPoints.Size = new System.Drawing.Size(171, 23);
+            this.buttonColorPoints.TabIndex = 14;
+            this.buttonColorPoints.Text = "Color Points";
+            this.buttonColorPoints.UseVisualStyleBackColor = true;
+            this.buttonColorPoints.Click += new System.EventHandler(this.buttonColorPoints_Click);
+            // 
+            // buttonColorDots
+            // 
+            this.buttonColorDots.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonColorDots.Location = new System.Drawing.Point(8, 241);
+            this.buttonColorDots.Name = "buttonColorDots";
+            this.buttonColorDots.Size = new System.Drawing.Size(171, 23);
+            this.buttonColorDots.TabIndex = 13;
+            this.buttonColorDots.Text = "Color Dots";
+            this.buttonColorDots.UseVisualStyleBackColor = true;
+            this.buttonColorDots.Click += new System.EventHandler(this.buttonColorDots_Click);
+            // 
+            // buttonColorBackground
+            // 
+            this.buttonColorBackground.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonColorBackground.Location = new System.Drawing.Point(8, 212);
+            this.buttonColorBackground.Name = "buttonColorBackground";
+            this.buttonColorBackground.Size = new System.Drawing.Size(171, 23);
+            this.buttonColorBackground.TabIndex = 12;
+            this.buttonColorBackground.Text = "Color Background";
+            this.buttonColorBackground.UseVisualStyleBackColor = true;
+            this.buttonColorBackground.Click += new System.EventHandler(this.buttonColorBackground_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 552);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reset Points";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBoxAnimation
+            // 
+            this.checkBoxAnimation.AutoSize = true;
+            this.checkBoxAnimation.Location = new System.Drawing.Point(99, 36);
+            this.checkBoxAnimation.Name = "checkBoxAnimation";
+            this.checkBoxAnimation.Size = new System.Drawing.Size(53, 19);
+            this.checkBoxAnimation.TabIndex = 10;
+            this.checkBoxAnimation.Text = "anim";
+            this.checkBoxAnimation.UseVisualStyleBackColor = true;
+            this.checkBoxAnimation.CheckedChanged += new System.EventHandler(this.checkBoxAnimation_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 314);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(181, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Cant Go To Same Point Again";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // buttonResetBM
+            // 
+            this.buttonResetBM.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonResetBM.Location = new System.Drawing.Point(12, 581);
+            this.buttonResetBM.Name = "buttonResetBM";
+            this.buttonResetBM.Size = new System.Drawing.Size(167, 23);
+            this.buttonResetBM.TabIndex = 7;
+            this.buttonResetBM.Text = "Reset BitMap";
+            this.buttonResetBM.UseVisualStyleBackColor = true;
+            this.buttonResetBM.Click += new System.EventHandler(this.buttonResetBM_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.Location = new System.Drawing.Point(12, 610);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(167, 23);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "Reset All";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
             // 
             // numericUpDown1
             // 
@@ -70,11 +180,6 @@
             2,
             0,
             0});
-            this.numericUpDown1.Minimum = new decimal(new int[] {
-            1000,
-            0,
-            0,
-            0});
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(179, 23);
             this.numericUpDown1.TabIndex = 5;
@@ -87,7 +192,7 @@
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(8, 47);
+            this.checkedListBox1.Location = new System.Drawing.Point(8, 58);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(171, 148);
             this.checkedListBox1.TabIndex = 4;
@@ -95,7 +200,7 @@
             // PointsCount
             // 
             this.PointsCount.AutoSize = true;
-            this.PointsCount.Location = new System.Drawing.Point(64, 29);
+            this.PointsCount.Location = new System.Drawing.Point(67, 37);
             this.PointsCount.Name = "PointsCount";
             this.PointsCount.Size = new System.Drawing.Size(13, 15);
             this.PointsCount.TabIndex = 3;
@@ -104,7 +209,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 29);
+            this.label1.Location = new System.Drawing.Point(11, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 15);
             this.label1.TabIndex = 2;
@@ -136,6 +241,7 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(800, 668);
@@ -147,11 +253,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(974, 674);
             this.Controls.Add(this.panelMainScreen);
             this.Controls.Add(this.panelSideMenu);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BitMapTest";
             this.panelSideMenu.ResumeLayout(false);
             this.panelSideMenu.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
@@ -171,5 +278,13 @@
         private Label PointsCount;
         private Label label1;
         private NumericUpDown numericUpDown1;
+        private Button buttonReset;
+        private Button buttonResetBM;
+        private CheckBox checkBox1;
+        private CheckBox checkBoxAnimation;
+        private Button button1;
+        private Button buttonColorPoints;
+        private Button buttonColorDots;
+        private Button buttonColorBackground;
     }
 }
--- BitMapTest/Form1.cs
+++ BitMapTest/Form1.cs
@@ -12,6 +12,11 @@ namespace BitMapTest
         private void init()
         {
             Bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            checkedListBox1.Items.Clear();
+            StartPointCollection.Clear();
+            PointCount = 0;
+            PointsCount.Text = "0";
+            pictureBox1.Image = Bitmap;
         }
 
         private Bitmap Bitmap;
@@ -21,6 +26,8 @@ namespace BitMapTest
         public int PointCount = 0;
 
         public PointF CurrientPoint;
+        public bool canGoToSameSpotAgain;
+        public bool simulationAnimated;
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             PointF point = new Point(e.X, e.Y);
@@ -28,21 +35,39 @@ namespace BitMapTest
             PointsCount.Text = PointCount.ToString();
             StartPointCollection.Add(StartPointCollection.Count, point);
             checkedListBox1.Items.Add(point);
+            Bitmap.SetPixel(e.X, e.Y, PointColor);
+            pictureBox1.Image= Bitmap;
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
             Random rnd = new Random();
+            int lastRNDnum = 0;
+            var nupSave = numericUpDown1.Value;
 
-            for (int i = 0; i < numericUpDown1.Value; i++)
+            for (int i = 0; i < nupSave; i++)
             {
                 int iRND = rnd.Next(0, StartPointCollection.Count);
-
+                
+                if(!(lastRNDnum == iRND && canGoToSameSpotAgain == true)) { 
                 PointF newP = ClacPoint(Bitmap, StartPointCollection[iRND], CurrientPoint);
                 DrawPoint(Bitmap, newP);
                 CurrientPoint = newP;
+                lastRNDnum = iRND;
+                    if (simulationAnimated && i % 100 == 0) animation(100);
+                    
+                }
             }
             pictureBox1.Image = Bitmap;
+            numericUpDown1.Value = nupSave;
+        }
+
+        private void animation(int value)
+        {
+            numericUpDown1.Value -= value;
+            numericUpDown1.Update();
+            pictureBox1.Image = Bitmap;
+            pictureBox1.Update();
         }
 
         private PointF ClacPoint(Bitmap btm, PointF pTo, PointF pCur)
@@ -58,9 +83,71 @@ namespace BitMapTest
         {
             if (p.X > btm.Width - 1) p.X = btm.Width - 1;
             if (p.Y > btm.Height - 1) p.Y = btm.Height - 1;
-            btm.SetPixel((int)p.X, (int)p.Y, Color.HotPink);
+            btm.SetPixel((int)p.X, (int)p.Y, DotColor);
 
         }
 
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void buttonResetBM_Click(object sender, EventArgs e)
+        {
+            Bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = Bitmap;
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //for(int i = 0;i < StartPointCollection.Count;i++)
+            //{
+            //    if ((PointF) checkedListBox1.CheckedItems[i] == StartPointCollection[i]) 
+            //    {
+            //        checkedListBox1.Items.Remove(StartPointCollection[i]);
+            //        StartPointCollection.Remove(i);
+            //        PointCount--;
+            //    } 
+            //}
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            canGoToSameSpotAgain = checkBox1.Checked;
+        }
+
+        private void checkBoxAnimation_CheckedChanged(object sender, EventArgs e)
+        {
+            simulationAnimated = checkBoxAnimation.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            StartPointCollection.Clear();
+            PointCount = 0;
+            PointsCount.Text = "0";
+        }
+
+        private void buttonColorBackground_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            pictureBox1.BackColor = colorDialog.Color;
+        }
+        public Color DotColor = Color.Black;
+        private void buttonColorDots_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            DotColor = colorDialog.Color;
+        }
+        public Color PointColor = Color.Red;
+        private void buttonColorPoints_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            PointColor = colorDialog.Color;
+        }
     } 
 }
